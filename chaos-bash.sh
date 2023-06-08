542856b51dc86174fc93a1e6455f83aeb8843a0a
Ahmed M <106396603+Hunt3r0x@users.noreply.github.com>
2023-06-08 19:32:29 +0300

    apylk
    حرفيا

--- chaos-bash.sh
+++ chaos-bash.sh
@@ -1,22 +1,74 @@
 #!/bin/bash
 
-output_folder="output"
-url="https://chaos-data.projectdiscovery.io/"
-endpoints="endpoints.txt" # must be updated every run
-programs="programs"
+JSON_FILE="index.json"
+JSON_URL="https://chaos-data.projectdiscovery.io/index.json"
+OLD_JSON_FILE="old_index.json"
+OUTPUT_DIR="output_directory"
 
-mkdir -p "$output_folder"
-mkdir -p "$programs"
+# Create the output directory if it doesn't exist
+if [ ! -d "$OUTPUT_DIR" ]; then
+  echo "Creating output directory..."
+  mkdir "$OUTPUT_DIR"
+fi
 
-for file in $(cat "$endpoints"); do
-    curl -sLO "$url/$file"
-done
+download_json_file() {
+  echo "Downloading JSON file: $JSON_URL"
+  if curl -s "$JSON_URL" -o "$JSON_FILE"; then
+    echo "Downloaded JSON file: $JSON_FILE"
+  else
+    echo "Failed to download JSON file."
+    return 1
+  fi
+}
 
-for file in *.zip; do
-    unzip -q "$file" -d "$programs"
-    rm "$file"
-done
+check_json_changes() {
+  if [ -f "$OLD_JSON_FILE" ]; then
+    current_updated=$(jq -r '.[].last_updated' "$JSON_FILE")
+    old_updated=$(jq -r '.[].last_updated' "$OLD_JSON_FILE")
 
-cat "$programs"/*/*.txt > "$output_folder"/subdomains.txt
+    if [ "$current_updated" = "$old_updated" ]; then
+      echo "No changes in JSON file. Skipping download."
+      return 0
+    fi
+  fi
 
-rm -rf "$programs"
+  mv "$JSON_FILE" "$OLD_JSON_FILE"
+}
+
+download_and_extract_urls_from_json() {
+  urls=$(jq -r '.[].URL' "$JSON_FILE")
+  for url in $urls; do
+    filename=$(basename "$url")
+    zip_filename="${filename%.*}.zip"
+    extracted_filename="${filename%.*}.txt"
+
+    # Check if the extracted file already exists
+    if [ -f "$OUTPUT_DIR/$extracted_filename" ]; then
+      echo "File already exists: $extracted_filename. Skipping download and extraction."
+      continue
+    fi
+
+    echo "Downloading: $filename"
+    if wget -q "$url" -O "$OUTPUT_DIR/$zip_filename"; then
+      echo "Downloaded: $zip_filename"
+      echo "Extracting: $zip_filename"
+      if unzip -qq -o "$OUTPUT_DIR/$zip_filename" "*.txt" -d "$OUTPUT_DIR"; then
+        echo "Extracted: $zip_filename"
+        rm "$OUTPUT_DIR/$zip_filename"
+      else
+        echo "Failed to extract: $zip_filename"
+      fi
+    else
+      echo "Failed to download: $filename"
+    fi
+  done
+}
+
+if [ ! -f "$JSON_FILE" ]; then
+  download_json_file
+fi
+
+if [ -f "$JSON_FILE" ]; then
+  check_json_changes
+  download_and_extract_urls_from_json
+fi
